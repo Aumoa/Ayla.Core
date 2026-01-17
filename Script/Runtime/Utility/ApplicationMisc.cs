20ab26714b914c7a2819a75598f0611b824c27ad
liberty <wkdghcjf1234@gmail.com>
2026-01-18 01:47:33 +0900

    IsInMainThread 구현 추가

---
+++ Script/Runtime/Utility/ApplicationMisc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Ayla
+{
+    public static class ApplicationMisc
+    {
+        private static int s_MainThreadId;
+
+#if UNITY_EDITOR
+        [InitializeOnLoadMethod]
+        private static void EditorStaticAwake()
+        {
+            s_MainThreadId = Environment.CurrentManagedThreadId;
+        }
+#endif
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void StaticAwake()
+        {
+            s_MainThreadId = Environment.CurrentManagedThreadId;
+        }
+
+        public static bool IsInMainThread()
+        {
+            return Environment.CurrentManagedThreadId == s_MainThreadId;
+        }
+    }
+}
