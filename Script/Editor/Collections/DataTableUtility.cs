484296c18c6655705ea44349ca683c3f894fc6e4
liberty <wkdghcjf1234@gmail.com>
2026-01-31 23:41:39 +0900

    DataTable Editor 더블클릭 지원

---
+++ Script/Editor/Collections/DataTableUtility.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace Ayla
+{
+    internal static class DataTableUtility
+    {
+        [OnOpenAsset]
+        public static bool OnOpenAsset(int instanceId, int line)
+        {
+            var obj = EditorUtility.EntityIdToObject(instanceId);
+            if (obj is DataTable)
+            {
+                var serializedObject = new SerializedObject(obj);
+                var targetProperty = serializedObject.FindProperty("m_Dict");
+                if (targetProperty == null)
+                {
+                    return false;
+                }
+
+                var window = ScriptableObject.CreateInstance<OrderedDictionaryEditor>();
+                window.SelectProperty(targetProperty);
+                window.Show();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
