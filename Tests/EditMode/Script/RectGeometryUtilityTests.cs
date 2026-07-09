8b9ef6e2b6307b77d941818d3147c64c55987593
liberty <wkdghcjf1234@gmail.com>
2026-07-09 23:03:52 +0900

    Test Classes 분리

--- Tests/EditMode/Script/RectGeometryUtilityTests.cs
+++ Tests/EditMode/Script/RectGeometryUtilityTests.cs
@@ -1,48 +1,10 @@
 #nullable enable
 
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 
 namespace Ayla.Tests
 {
-    public sealed class CollectionUtilityTests
-    {
-        [Test]
-        public void AddRange_AppendsItemsInEnumerationOrder()
-        {
-            var list = new List<int> { 1 };
-
-            list.AddRange(new[] { 2, 3 });
-
-            CollectionAssert.AreEqual(new[] { 1, 2, 3 }, list);
-        }
-
-        [Test]
-        public void IsValidIndex_SupportsForwardAndFromEndIndices()
-        {
-            IReadOnlyList<string> list = new[] { "first", "second", "third" };
-
-            Assert.That(list.IsValidIndex(0), Is.True);
-            Assert.That(list.IsValidIndex(2), Is.True);
-            Assert.That(list.IsValidIndex(3), Is.False);
-            Assert.That(list.IsValidIndex(^1), Is.True);
-            Assert.That(list.IsValidIndex(^3), Is.True);
-            Assert.That(list.IsValidIndex(^0), Is.False);
-        }
-
-        [Test]
-        public void GetOrDefaultAt_ReturnsFallbackForOutOfRangeIndices()
-        {
-            IReadOnlyList<string> list = new[] { "first", "second", "third" };
-
-            Assert.That(list.GetOrDefaultAt(1, "fallback"), Is.EqualTo("second"));
-            Assert.That(list.GetOrDefaultAt(^1, "fallback"), Is.EqualTo("third"));
-            Assert.That(list.GetOrDefaultAt(-1, "fallback"), Is.EqualTo("fallback"));
-            Assert.That(list.GetOrDefaultAt(^0, "fallback"), Is.EqualTo("fallback"));
-        }
-    }
-
     public sealed class RectGeometryUtilityTests
     {
         [Test]
